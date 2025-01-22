6b6b9717c60ff17bcb95407154ea9c8e9a982b5d
MHH77 <mesfandiari77@gmail.com>
2025-01-22 06:59:40 +0330

    [feature] Solve the third challenge

---
+++ Challenge_3.js
@@ -0,0 +1,7 @@
+const scoreDolphins=(97+112+81)/3;
+const scoreKoalas=(109+95+86)/3;
+if (scoreDolphins > scoreKoalas) {
+    console.log("Dolphons win the trophy");
+} else {
+    console.log("Dolphons win the trophy");
+}
