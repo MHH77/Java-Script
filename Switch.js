1f8af31ab9e18e5806ddeea079d165bdac699d6a
MHH77 <mesfandiari77@gmail.com>
2025-01-25 06:32:52 +0330

    [feature] Switch statement

---
+++ Switch.js
@@ -0,0 +1,27 @@
+let day = 3;
+
+switch (day) {
+  case 1:
+    console.log("شنبه");
+    break;
+  case 2:
+    console.log("یک‌شنبه");
+    break;
+  case 3:
+    console.log("دوشنبه");
+    break;
+  case 4:
+    console.log("سه‌شنبه");
+    break;
+  case 5:
+    console.log("چهارشنبه");
+    break;
+  case 6:
+    console.log("پنج‌شنبه");
+    break;
+  case 7:
+    console.log("جمعه");
+    break;
+  default:
+    console.log("عدد وارد شده برای روز معتبر نیست.");
+}
