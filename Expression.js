5c65199e283c7738e9f8aa8247610f0baa24978b
MHH77 <mesfandiari77@gmail.com>
2025-01-27 06:47:06 +0330

    [feature] Expression in js

---
+++ Expression.js
@@ -0,0 +1,16 @@
+let result = 5 + 10;  // 5 + 10 یک expression است که نتیجه آن 15 می‌شود
+console.log(result);  // چاپ می‌کند 15
+
+
+let isEqual = (x === y);  // x === y یک expression است که نتیجه آن true یا false است
+console.log(isEqual);
+
+let square = (a) => a * a;  // این یک expression است که تابعی ایجاد می‌کند
+console.log(square(4));  // چاپ می‌کند 16
+
+let isAdult = (age >= 18 && age <= 65);  // یک expression که نتیجه‌ی آن true یا false است
+console.log(isAdult);
+
+let max = (a > b) ? a : b;  // این یک expression است که مقدار max را به‌طور شرطی انتخاب می‌کند
+console.log(max);  // چاپ می‌کند بزرگترین عدد بین a و b
+
