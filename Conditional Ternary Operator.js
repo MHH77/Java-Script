cc1e5e9ee6839bf2f2cf92a59def9983aad89c1d
MHH77 <mesfandiari77@gmail.com>
2025-01-27 07:02:33 +0330

    [feature] Conditional Ternary Operator in js

---
+++ Conditional Ternary Operator.js
@@ -0,0 +1,5 @@
+let age = 20;
+let isMarried = false;
+let canDrive = age >= 18 ? (isMarried ? "Can drive" : "Can drive, but may have more responsibilities") : "Cannot drive";
+
+console.log(canDrive);
