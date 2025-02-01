93d8b867a04346b6ae7de7976779fa697c7cf4ed
MHH77 <mesfandiari77@gmail.com>
2025-02-01 06:45:09 +0330

    [feature] Implement function in js

---
+++ Function.js
@@ -0,0 +1,11 @@
+function greet(name) {
+    return "Hi " + name + "!";
+}
+
+console.log(greet("Ali"));
+
+function operation(num1, num2) {
+    return num1 + num2;
+}
+const sum = operation(2, 3);
+console.log(`result is => ${sum}`);
