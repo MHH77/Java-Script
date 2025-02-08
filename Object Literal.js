cdd496b66a79f05ea12ae4136a7023cd388cbdd4
MHH77 <mesfandiari77@gmail.com>
2025-02-08 06:42:54 +0330

    Object definition in js

---
+++ Object Literal.js
@@ -0,0 +1,11 @@
+let person = {
+    firstName: "Ali",
+    lastName: "Ahmadi",
+    age: 30,
+    isMarried: false,
+};
+
+console.log(person.firstName);
+console.log(person["lastName"]);
+console.log(person.age);
+console.log(person.isMarried);
